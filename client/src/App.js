957bcd4180c1c2fc5980ac39adfeffb0a42fc069
KaoParunyu <kao192837@gmail.com>
2023-09-07 00:16:42 +0700

    hello

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+
+//import Container from 'react-bootstrap/Container';
+import Login from './Login';
+//import Navbar from 'react-bootstrap/Navbar';
+
+function OffcanvasExample() {
+  return (
+    <>
+    
+    <div>
+      <Login/>
+      
+      </div>
+      {/*
+<Navbar bg="primary" data-bs-theme="dark" >
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>*/}
+    </>
+  );
+}
+
+export default OffcanvasExample;
