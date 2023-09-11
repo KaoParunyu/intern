c968a8d39050bf97cde90ac1c1bee8f05cbad820
KaoParunyu <kao192837@gmail.com>
2023-09-11 17:54:43 +0700

    gmm11/9/66

--- server/app.js
+++ server/app.js
@@ -43,7 +43,8 @@ app.post('/login', jsonParser, function (req, res, next) {
          if (users.length == 0) { res.json({status: 'error', message: 'no user found'}); return }
         bcrypt.compare(req.body.password, users[0].password, function(err, isLogin) {
             if (isLogin) {
-                res.json({status: 'ok', message: 'login success'})
+                var token = jwt.sign({ email: users[0].email }, secret, {expiresIn: '1h'} );
+                res.json({status: 'ok', message: 'login success',token})
             }else {
                 res.json({status: 'error', message: 'login failed'})
             }
