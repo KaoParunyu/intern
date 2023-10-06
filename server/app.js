d5160432c4e94f1a37ae26caaa39a82b046233e3
KaoParunyu <kao192837@gmail.com>
2023-10-06 17:48:34 +0700

    6/10/66

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "12345678",
   database: "project",
 });
 
